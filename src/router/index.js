e1e075bb724a1e8aa90bded6c7a88a880156da8b
ww-yy-101 <158478113+ww-yy-101@users.noreply.github.com>
2024-09-03 20:30:51 +0800

    Update index.js
    3更改

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,6 @@
 import { useUserStore } from '@/stores'
 import { createRouter, createWebHistory } from 'vue-router'
 
-// createRouter 创建路由实例
-// 配置 history 模式
-// 1.history模式：createWebHistory //地址栏不带 #
-// 2.hash模式：createWebHashHistory //地址栏带 #
-
-// console.log(import.meta.env.DEV) 是否为开发环境
-
 // Vite 中的环境变量 import.meta.env.BASE_URL 就是vite.config.js 中的 base 配置项
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -45,13 +38,7 @@ const router = createRouter({
 
 // 登录访问拦截  => 默认直接放行的
 // 根据返回值决定，是放行还是拦截
-// 返回值：
-// 1.undefined 或 true  直接放行
-// 2.false 拦回from的地址页面
-// 3.具体路径 或 路径对象  拦截到对应的地址
-//  '/login'  {name: 'login'}
 router.beforeEach((to) => {
-  // 如果没有token，且访问的是否为登录页，拦截到登录，其他情况正常放行
   const useStore = useUserStore()
   if (!useStore.token && to.path !== '/login') return '/login'
 
